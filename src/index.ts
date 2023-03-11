f85bc2c5101ecbcb40d36eb4b39abb5c20217a1b
maeta187 <nexus18789@gmail.com>
2023-03-11 13:57:47 +0900

    🩹 : 不要な関数のコメント化

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import { checkObjectType } from './Object'
-import { cheekArrayType, checkArrayMap } from './Array'
-import Tuple from './Tuple'
-import { cheekLiteral } from './Literal'
-import { checkFunction, arrowFunction, callbackFunction } from './Function'
+// import { checkObjectType } from './Object'
+// import { checkArrayType, checkArrayMap } from './Array'
+// import Tuple from './Tuple'
+// import { checkLiteral } from './Literal'
+// import { checkFunction, arrowFunction, callbackFunction } from './Function'
+import { checkUnknown } from './Unknown'
 
-checkObjectType()
-cheekArrayType()
-checkArrayMap()
-Tuple()
-cheekLiteral()
-checkFunction()
-arrowFunction('arrowFunction')
-callbackFunction(2, (cbFunction) => {
-  return cbFunction * 10
-})
+// checkObjectType()
+// checkArrayType()
+// checkArrayMap()
+// Tuple()
+// checkLiteral()
+// checkFunction()
+// arrowFunction('arrowFunction')
+// callbackFunction(2, (cbFunction) => {
+//   return cbFunction * 10
+// })
+checkUnknown()
