6de918eb67b93b4181023fd9678c3447aed37b8c
maeta187 <nexus18789@gmail.com>
2023-03-17 08:38:13 +0900

    ⚡️ : ageのPrivate化

--- src/Class/index.ts
+++ src/Class/index.ts
@@ -1,9 +1,16 @@
 class Person {
-  name: string
-  // Classのプロパティはconstructor()に書かれたもの
-  constructor(initName: string) {
-    // thisは実行された場所で依存する(変わる)
-    this.name = initName
+  // name: string
+  // private age: number
+  /**
+   * Classのプロパティはconstructor()に書かれたもの
+   * constructor()の引数に書くことで初期化を省力でき、constructor()の上部に書いていた型定義も不要
+   */
+  constructor(public name: string, private age: number) {
+    /**
+     * thisは実行された場所で依存する(変わる)
+     * this.name = initName
+     * this.age = initAge
+     */
   }
 
   // TypeScriptは第一引数にthisを伝えることができる(第一引数にしか書けない)
@@ -12,23 +19,28 @@ class Person {
   // }
   // Class作り出すインスタンスを表す型を同時に作る
   greeting(this: Person) {
-    console.log(`Hello! My name is ${this.name}`)
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`)
   }
   // アロー関数は定義時に決まる(インスタンス化)
   // greeting = () => {
   //   console.log(`Hello! My name is ${this.name}`)
   // }
+
+  incrementAge() {
+    this.age++
+  }
 }
 
-const person = new Person('Quill')
+const person = new Person('Quill', 30)
 
-const anotherQuill = {
-  // nameが無いとgreeting()で参照するthis.nameはundefinedとなる
-  name: 'anotherQuill',
-  greeting: person.greeting
-}
+// const anotherQuill = {
+//   // nameが無いとgreeting()で参照するthis.nameはundefinedとなる
+//   name: 'anotherQuill',
+//   greeting: person.greeting.bind(person),
+//   incrementAge: person.incrementAge.bind(person)
+// }
 export const checkClass = () => {
   // console.log(person)
   person.greeting()
-  anotherQuill.greeting()
+  // anotherQuill.greeting()
 }
