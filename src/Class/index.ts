49772c032efd313a0648f9af6973dca176e8c607
maeta187 <nexus18789@gmail.com>
2023-03-19 18:38:22 +0900

    ⚡️ : Personクラスをabstractクラスに変更

--- src/Class/index.ts
+++ src/Class/index.ts
@@ -1,18 +1,27 @@
-class Person {
+/**
+ * Abstract Classとするとインスタンスが作成できない
+ * 継承のみのClassとなる
+ */
+abstract class Person {
+  static species = 'Homo sapiens'
+  static isAdult(age: number) {
+    if (age > 17) return true
+    return false
+  }
   // name: string
   // private age: number
   /**
    * Classのプロパティはconstructor()に書かれたもの
    * constructor()の引数に書くことで初期化を省力でき、constructor()の上部に書いていた型定義も不要
    */
-  constructor(public name: string, private age: number) {
+  readonly id: number = 32
+  constructor(public readonly name: string, protected age: number) {
     /**
      * thisは実行された場所で依存する(変わる)
      * this.name = initName
      * this.age = initAge
      */
   }
-
   // TypeScriptは第一引数にthisを伝えることができる(第一引数にしか書けない)
   // greeting(this: { name: string }) {
   //   console.log(`Hello! My name is ${this.name}`)
@@ -20,12 +29,13 @@ class Person {
   // Class作り出すインスタンスを表す型を同時に作る
   greeting(this: Person) {
     console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`)
+    this.explainJob()
   }
   // アロー関数は定義時に決まる(インスタンス化)
   // greeting = () => {
   //   console.log(`Hello! My name is ${this.name}`)
   // }
-
+  abstract explainJob(): void
   incrementAge() {
     this.age++
   }
