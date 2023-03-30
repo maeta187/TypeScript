8f296a740b6afe91c294fd640b263716e773397c
maeta187 <nexus18789@gmail.com>
2023-03-31 08:40:21 +0900

    ✨ : タイプガードの復習

--- src/Apply/index.ts
+++ src/Apply/index.ts
@@ -27,3 +27,70 @@ type StringNumber = string | number
  * 重複が無ければnever型となり、複数の重複があるとユニオン型になる
  */
 // type Mix = NumberBoolean & StringNumber
+
+// タイプガード
+/**
+ * typeof演算子
+ */
+function toUpperCase(x: string | number) {
+  if (typeof x === 'string') {
+    return x.toUpperCase()
+  }
+  return ''
+}
+
+/**
+ * in演算子
+ */
+type NomadWorker = Engineer | Blogger
+
+const worker = {
+  name: 'Quill',
+  role: 'front-end'
+}
+
+function describeProfile(nomadWorker: NomadWorker) {
+  console.log(nomadWorker.name)
+  if ('role' in nomadWorker) {
+    // この処理に入った時にnomadWorkerの型はEngineerとなる
+    console.log(nomadWorker.role)
+  }
+  if ('follower' in nomadWorker) {
+    // この処理に入った時にnomadWorkerの型はBloggerとなる
+    console.log(nomadWorker.follower)
+  }
+}
+
+class Dog {
+  speak() {
+    console.log('bow-wow')
+  }
+}
+
+class Bird {
+  speak() {
+    console.log('tweet-tweet')
+  }
+  fly() {
+    console.log('flutter')
+  }
+}
+
+/**
+ * instanceof演算子
+ * instanceofを使用する場合はnewで作られたClassのインスタンスである必要がある
+ */
+type Pet = Dog | Bird
+function havePet(pet: Pet) {
+  pet.speak()
+  if (pet instanceof Bird) {
+    pet.fly()
+  }
+}
+
+export const checkApply = () => {
+  console.log(quill)
+  console.log(toUpperCase('quill'))
+  describeProfile(worker)
+  havePet(new Bird())
+}
