bfc0174b3169fdc0454e86bfbb7f196f28ddeb25
maeta187 <nexus18789@gmail.com>
2023-05-07 19:25:54 +0900

    ✨ : Genericsの学習

--- src/Generics/index.ts
+++ src/Generics/index.ts
@@ -77,6 +77,54 @@ fetchData.then((data) => {
  */
 const vegetables: Array<string> = ['Tomato', 'Broccoli', 'Asparagus']
 
+/**
+ * イコールを使用することでデフォルトの型定義を行うことができる
+ */
+interface ResponseData<T extends { message: string } = never> {
+  data: T
+  status: number
+}
+
+let tmp2: ResponseData
+
+interface Vegetables {
+  tomato: string
+  pumpkin: string
+}
+
+/**
+ * MappedType
+ * ユニオン型のように書くか、keyofを使用して反復処理を行うことができる
+ * readonlyやエクスクラメーションにマイナスを付けるとそれぞれの機能を外すことができる
+ */
+type MappedType = {
+  readonly [K in keyof Vegetables]?: string
+  // [P in 'tomato' | 'pumpkin']: string
+}
+
+/**
+ * ConditionalTypes
+ * tomatoがstring型に代入できる時はnumber、代入できない時はbooleanとなる
+ */
+type ConditionalTypes = 'tomato' extends string ? number : boolean
+
+/**
+ * infer
+ * inferはanyと同様でなんでも許容する
+ * 継承させている値に最も近い形で型推論される
+ */
+type ConditionalTypesInfer = { tomato: 'tomato' } extends { tomato: infer R }
+  ? R
+  : boolean
+
+/**
+ * DistributiveConditional
+ * 継承している型とそれぞれ検証を行って型定義をする
+ */
+type DistributiveConditionalTypes<T> = T extends 'tomato' ? number : boolean
+
+let tmp3: DistributiveConditionalTypes<'tomato' | 'pumpkin'>
+
 export const checkGenerics = () => {
   console.log(copy({ name: 'Quill' }))
   console.log(copyKey({ name: 'Quill', age: 30 }, 'name'))
